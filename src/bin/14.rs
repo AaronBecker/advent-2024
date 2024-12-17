7810326ca8b9d7397bc6768e9ac935c13f63dc24
Aaron Becker <aaron@falsesharing.com>
2024-12-16 22:59:36 -0600

    Remove non-functional christmas tree finding test

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -77,8 +77,5 @@ mod tests {
     }
 
     #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(6577));
-    }
+    fn test_part_two() {}
 }
